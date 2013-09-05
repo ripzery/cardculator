821e80b880fd8383efe8a4dce0b3d9fb38bd8dc3
ripzery@gmail.com <ripzery@gmail.com@31627ee4-d90a-121b-8c85-312390247f3e>
2013-09-05 16:19:15 +0000

    1.try to combine match color 2.try to place component suitable place

--- src/Menu.java
+++ src/Menu.java
@@ -1,18 +1,33 @@
 import javax.swing.*;
 import javax.swing.UIManager.*;
 import java.awt.*;
+import static java.awt.Component.CENTER_ALIGNMENT;
+import net.miginfocom.swing.MigLayout;
 
 public class Menu extends JFrame{
     private int width=800,height=600;
     private JPanel main,buttonGroup;
     private JLabel heading;
-    private JButton b1;
+    private JButton bstart,bhigh,bexit;
+    private GridBagConstraints c;
     public Menu(){
+        
+       
         addComponent();
-        setLayout(null);
-        add(main);
+        c = new GridBagConstraints();
+        GridBagLayout frame_layout = new GridBagLayout();
+        setLayout(frame_layout);
+        
+        c.gridy = 0;
+        add(main,c);
+        c.gridy = 1;
+        c.insets = new Insets(120,0,0,0);
+        add(buttonGroup,c);
+ 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocation(320,70);
         setSize(width,height);
+        getContentPane().setBackground(new Color(0x3e,0x60,0x6f));
         setVisible(true);
         setTitle("Cardculator");
     }
@@ -20,7 +35,7 @@ public class Menu extends JFrame{
         /*
          * Using UIManager package make the program good look and feel "Nimbus" :)
          */    
-        try {
+        /*try {
             for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
                     UIManager.setLookAndFeel(info.getClassName());
@@ -29,28 +44,42 @@ public class Menu extends JFrame{
             }
         } catch (Exception e) {
             // If Nimbus is not available, you can set the GUI to another look and feel.
-        }
+        }*/
           new Menu(); 
     }
     
     public void addComponent(){
         main = new JPanel();
         main.setLayout(new FlowLayout());
-        main.setBounds(0, 25, width, height);
-        
-        //main.setSize(400,400);
-        /*
-         * Example of create button in nimbus size configuration
-         * b1 = new JButton("HELLOSDADSAD");
-         * b1.setPreferredSize(new Dimension(200,100));
-         * b1.putClientProperty("JComponent.sizeVariant", "regular");
-         */
- 
-        heading = new JLabel("<html><p>Welcome to Cardculator game!</p></html>");
-        Font h = new Font("Arial",Font.BOLD,20);
+        heading = new JLabel("Welcome to Cardculator game!");
+        Font h = new Font("Arial",Font.BOLD,36);
         heading.setFont(h);
-        heading.setForeground(Color.black);
+        heading.setForeground(new Color(0x91,0xaa,0x9d));
         main.add(heading);
+        main.setOpaque(false);
+        buttonGroup = new JPanel();
+        
+        buttonGroup.setLayout(new BorderLayout(50,50));
+        bstart = new JButton("Start game");
+        bstart.setPreferredSize(new Dimension(200,50));
+        bstart.setForeground(new Color(0x19,0x34,0x41));
+        bstart.setBackground(new Color(0x91,0xAA,0x9D));
+        buttonGroup.add(bstart,BorderLayout.NORTH);
+       
+        
+        bhigh = new JButton("High score");
+        bhigh.setPreferredSize(new Dimension(200,50));
+        bhigh.setForeground(new Color(0x19,0x34,0x41));
+        bhigh.setBackground(new Color(0x91,0xAA,0x9D));
+        buttonGroup.add(bhigh,BorderLayout.CENTER);
+        
+        bexit = new JButton("Exit");
+        bexit.setPreferredSize(new Dimension(200,50));
+        bexit.setForeground(new Color(0x19,0x34,0x41));
+        bexit.setBackground(new Color(0x91,0xAA,0x9D));
+        buttonGroup.add(bexit,BorderLayout.SOUTH);
+        buttonGroup.setOpaque(false);
+
     }
     
     public void addListener(){
