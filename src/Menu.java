b014080e0ff92a56a6ce98463028b1d4a3d63c66
ripzery@gmail.com <ripzery@gmail.com@31627ee4-d90a-121b-8c85-312390247f3e>
2013-09-08 07:36:53 +0000

    1.using Firenze Theme

--- src/Menu.java
+++ src/Menu.java
@@ -25,14 +25,14 @@ public class Menu extends JFrame{
         c.gridy = 0;
         add(main,c);
         c.gridy = 1;
-        c.insets = new Insets(120,0,0,0);
+        c.insets = new Insets(80,0,0,0);
         add(buttonGroup,c);
         addListener();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocation(320,70);
         this.pack();
         setSize(width,height);
-        getContentPane().setBackground(new Color(0x3e,0x60,0x6f));
+        getContentPane().setBackground(new Color(0xff,0xf0,0xa5));
         setVisible(true);
         setTitle("Cardculator");
     }
@@ -45,7 +45,6 @@ public class Menu extends JFrame{
                       break;
                   }
               }
-              
           }
           catch(Exception e){}
           new Menu();
@@ -57,7 +56,7 @@ public class Menu extends JFrame{
         heading = new JLabel("Welcome to Cardculator game!");
         Font h = new Font("Arial",Font.BOLD,36);
         heading.setFont(h);
-        heading.setForeground(new Color(0x91,0xaa,0x9d));
+        heading.setForeground(new Color(0xb6,0x49,0x26));
         main.add(heading);
         main.setOpaque(false);
         buttonGroup = new JPanel();
@@ -65,14 +64,11 @@ public class Menu extends JFrame{
         buttonGroup.setLayout(new BorderLayout(0,50));
         
         bstart = new myButton("Start game");
-        bstart.setFont(new Font("Arial",Font.BOLD,20));
-        Icon img = new ImageIcon("Minion-Happy-icon.png") ;  
-       // Image newimg = img.getScaledInstance( 64, 64,  java.awt.Image.SCALE_SMOOTH ) ;  
-        //ImageIcon icon = new ImageIcon( newimg );
+        bstart.setFont(new Font("Arial",Font.BOLD,25));
+        Icon img = new ImageIcon("Minion-Happy-icon.png");  
         bstart.setIcon(img);
         bstart.setBorder(BorderFactory.createEmptyBorder(0, -10, 2, 2));
         bstart.setIconTextGap(10);
-        
         buttonGroup.add(bstart,BorderLayout.NORTH);
        
         
@@ -80,7 +76,7 @@ public class Menu extends JFrame{
         img = new ImageIcon("Minion-Reading-icon.png");
         bhigh.setIcon(img);
         bhigh.setIconTextGap(10);
-        bhigh.setFont(new Font("Arial",Font.BOLD,20));
+        bhigh.setFont(new Font("Arial",Font.BOLD,25));
         bhigh.setBorder(BorderFactory.createEmptyBorder(0, -10, 2, 2));
         buttonGroup.add(bhigh,BorderLayout.CENTER);
         
@@ -88,12 +84,11 @@ public class Menu extends JFrame{
         img = new ImageIcon("Minion-Hello-icon.png");
         bexit.setIcon(img);
         bexit.setIconTextGap(10);
-        bexit.setFont(new Font("Arial",Font.BOLD,20));
-        bexit.setBorder(BorderFactory.createEmptyBorder(0, -20, 2, 2));
+        bexit.setFont(new Font("Arial",Font.BOLD,25));
+        bexit.setBorder(BorderFactory.createEmptyBorder(0, -15, 2, 2));
         buttonGroup.add(bexit,BorderLayout.SOUTH);
         
         buttonGroup.setOpaque(false);
-
     }
     public void addListener(){
         
@@ -103,23 +98,9 @@ public class Menu extends JFrame{
     public class myButton extends JButton{
         public myButton(String name){
             super(name);
-            setPreferredSize(new Dimension(200,80));
+            setPreferredSize(new Dimension(250,100));
             setForeground(new Color(0x19,0x34,0x41));
-            setBackground(new Color(0x91,0xAA,0x9D));
-            
-            addButtonListener();
-        }
-        
-        
-        public void addButtonListener(){
-            addMouseListener(new MouseAdapter(){
-                public void mouseEntered(MouseEvent e){
-                    
-                }
-                public void mouseExited(MouseEvent e){
-                    
-                }
-            });
+            setBackground(new Color(0xff,0xb0,0x3b));
         }
     }
 }
